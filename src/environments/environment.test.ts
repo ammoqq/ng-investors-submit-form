e0a561e19fc32f72dfab763d6ba88b475ec9c917
Paweł Dziennik <dziennikp@gmail.com>
2018-09-08 16:23:17 +0200

    added testing env

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAD8WiSjys6ZSYfGtGarhuV1cefzv_b6HY',
+    authDomain: "prime-test-3a0ac.firebaseapp.com",
+    databaseURL: "https://prime-test-3a0ac.firebaseio.com",
+    projectId: 'prime-test-3a0aca',
+    storageBucket: "prime-test-3a0ac.appspot.com",
+    messagingSenderId: "741799982279"
+  }
+};
